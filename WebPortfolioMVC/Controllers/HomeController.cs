919b12beeb6b3d6a2e5d81aad690184a1a2cf782
Max Baulin <baulin.maksim.1992@gmail.com>
2023-03-23 17:31:27 -0700

    Add project files.

---
+++ WebPortfolioMVC/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebPortfolioMVC.Models;
+
+namespace WebPortfolioMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
